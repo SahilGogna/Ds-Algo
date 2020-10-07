8fe4f7ed8bd7551d1f7eaadb6cafc1f86c413200
Sahil Gogna <gogna.sahil95@gmail.com>
2020-10-07 15:24:09 -0400

    folder structure refactoring

---
+++ Learning Microservices - 2/user-authetication/src/main/java/com/example/microservices/userauthetication/UserAutheticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservices.userauthetication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAutheticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAutheticationApplication.class, args);
+	}
+
+}
---
+++ Learning Microservices - 2/user-authetication/src/test/java/com/example/microservices/userauthetication/UserAutheticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservices.userauthetication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAutheticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
