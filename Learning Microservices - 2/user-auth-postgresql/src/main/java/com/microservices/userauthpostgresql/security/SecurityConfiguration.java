6375a2dbb1d8348a93028e694a4d7fbdcd660370
Sahil Gogna <gogna.sahil95@gmail.com>
2020-10-12 15:12:50 -0400

    jwt project added

--- Learning Microservices - 2/user-auth-postgresql/src/main/java/com/microservices/userauthpostgresql/security/SecurityConfiguration.java
+++ Learning Microservices - 2/user-auth-postgresql/src/main/java/com/microservices/userauthpostgresql/security/SecurityConfiguration.java
@@ -15,6 +15,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 	@Autowired
 	UserDetailsService userDetailsService;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService);
